cc3d6753a894e6f96326a87dd165a84442a9530a
MrXyfir <mr@xyfir.com>
2019-07-07 14:48:15 -0700

    Sort A-Z by id

--- build.js
+++ build.js
@@ -1,10 +1,21 @@
 const { writeFileSync, readFileSync } = require('fs');
 
-const recipes = JSON.parse(readFileSync('./recipes.json', 'utf8'));
+let recipes = JSON.parse(readFileSync('./recipes.json', 'utf8'));
+
+// Sort A-Z by id
+recipes = recipes.sort((a, b) => {
+  if (a.id < b.id) return -1;
+  if (a.id > b.id) return 1;
+  return 0;
+});
+
+// Minify
 const min = recipes.map(recipe => ({
   a: recipe.authors,
   s: recipe.series,
   b: recipe.books,
   i: recipe.id
 }));
+
+writeFileSync('./recipes.json', JSON.stringify(recipes, null, 2));
 writeFileSync('./recipes.min.json', JSON.stringify(min));
